58322d9e529bc1aa3ccff67b6a62f752531699dd
aleksey <maylo.kondratev@yandex.ru>
2020-03-17 17:50:58 +0300

    Add new files

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,52 @@
 $(function() {
-  $('.team-slider__slider').slick({
+  var $teamSlider = $('.team-slider__slider'),
+      $teamSlides = $teamSlider.find('.team__item');
+  $teamSlider.slick({
     arrows: true,
     dots: false,
     infinite: true,
     centerMode: true,
+    centerPadding: '240px',
     focusOnSelect: false,
-    slidesToShow: 5,
+    slidesToShow: 3,
     slidesToScroll: 1,
     prevArrow: '.team-slider__arrow_prev',
-    nextArrow: '.team-slider__arrow_next'
+    nextArrow: '.team-slider__arrow_next',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
   });
+  $teamSlider.slick('slickGoTo', 2);
+  $teamSlides.on('click', function() {
+    $teamSlider.slick('slickGoTo', this.dataset.slickIndex);
+  })
+
+  var $faqs = document.querySelector('.js-faqs');
+  if ($faqs) {
+    var $faqsTtl = [].slice.call($faqs.querySelectorAll('.faqs__title'));
+    var $faqsItem = [].slice.call($faqs.querySelectorAll('.faqs__item'));
+
+    $faqsTtl.forEach(function (el) {
+      el.addEventListener('click', function() {
+        classie.toggle(this.parentNode, '_opened');
+      })
+    });
+  }
 });
