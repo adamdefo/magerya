b3f709b5db43ebf814e81df4b0a9031b1f2e6022
aleksey <maylo.kondratev@yandex.ru>
2020-03-24 13:14:38 +0300

    Fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,12 @@
 $(function() {
   var $teamSlider = $('.team-slider__slider'),
       $teamSlides = $teamSlider.find('.team__item');
+  
+  var $teamCaption = $('.team-slider__caption'),
+    captionLabel = $teamCaption.find('.label'),
+    captionTtl = $teamCaption.find('.ttl'),
+    captionBtn = $teamCaption.find('.btn');
+
   $teamSlider.slick({
     arrows: true,
     dots: false,
@@ -18,7 +24,7 @@ $(function() {
         settings: {
           arrows: false,
           centerMode: true,
-          centerPadding: '40px',
+          centerPadding: '20px',
           slidesToShow: 3
         }
       },
@@ -27,7 +33,7 @@ $(function() {
         settings: {
           arrows: false,
           centerMode: true,
-          centerPadding: '40px',
+          centerPadding: '0px',
           slidesToShow: 1
         }
       }
@@ -36,13 +42,22 @@ $(function() {
   $teamSlider.slick('slickGoTo', 2);
   $teamSlides.on('click', function() {
     $teamSlider.slick('slickGoTo', this.dataset.slickIndex);
-  })
+  });
+  $teamSlider.on('afterChange', function(event, slick, currentSlide, nextSlide){
+    var slide = slick.$slides[currentSlide];
+    captionLabel.text(slide.dataset.post);
+    captionTtl.text(slide.dataset.name);
+    if (slide.dataset.link) {
+      captionBtn.attr('href', slide.dataset.link);
+      captionBtn.show();
+    } else {
+      captionBtn.hide();
+    }
+  });
 
   var $faqs = document.querySelector('.js-faqs');
   if ($faqs) {
     var $faqsTtl = [].slice.call($faqs.querySelectorAll('.faqs__title'));
-    var $faqsItem = [].slice.call($faqs.querySelectorAll('.faqs__item'));
-
     $faqsTtl.forEach(function (el) {
       el.addEventListener('click', function() {
         classie.toggle(this.parentNode, '_opened');
